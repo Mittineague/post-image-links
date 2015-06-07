e0cecbafaeef40f0f5c1621d2e073350d13372e0
Mittineague <allanh62@hotmail.com>
2015-06-07 03:44:07 -0400

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,8 @@
 # name: post-image-links
-# about: show links of images in posts
+# about: show linked filenames of images in posts
 # version: 0.1
 # authors: Mittineague
-# url: https://localhost:4000
+# url: https://github.com/Mittineague/post-image-links.git
 
 register_asset "javascripts/helpers/post-image-links.js.es6"
 register_asset "javascripts/discourse/templates/connectors/in-cooked/post-image-links.hbs"
